d9190e91c66e3acdcee8c6f0c8742d795beb65b1
rion <riiiiion@gmail.com>
2024-12-04 01:38:08 +0900

    シェルスクリプトにaws_session_managerのインストールを追加 その他微修正

--- mac/setup.sh
+++ mac/setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/zsh
 
 set -e # エラーが発生した場合にスクリプトを終了
 
@@ -48,14 +48,64 @@ else
 fi
 
 echo "===== ターミナル補完拡張のインストール ====="
-if [ ! -d "${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions" ]; then
+PLUGIN_DIR="${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions"
+
+if [ ! -d "$PLUGIN_DIR" ]; then
   echo "zsh-autosuggestionsをインストール中..."
-  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+  git clone https://github.com/zsh-users/zsh-autosuggestions "$PLUGIN_DIR"
   sed -i '' '/^plugins=/s/)/ zsh-autosuggestions)/' ~/.zshrc
   source ~/.zshrc
 else
-  echo "zsh-autosuggestionsはすでにインストールされています"
+  echo "zsh-autosuggestionsはすでにインストールされています。更新を確認中..."
+  CURRENT_DIR=$(pwd)
+  cd "$PLUGIN_DIR"
+  git pull
+  cd "$CURRENT_DIR"
 fi
+
+echo "===== aws_session_managerのインストール ====="
+echo "Session Manager Pluginをダウンロード中..."
+curl "https://s3.amazonaws.com/session-manager-downloads/plugin/latest/mac_arm64/sessionmanager-bundle.zip" -o "sessionmanager-bundle.zip"
+
+if [ $? -ne 0 ]; then
+    echo "Failed to download sessionmanager-bundle.zip"
+    exit 1
+fi
+
+# ZIPファイルの解凍
+echo "ZIPファイルの解凍中..."
+unzip sessionmanager-bundle.zip
+
+if [ $? -ne 0 ]; then
+    echo "Failed to unzip sessionmanager-bundle.zip"
+    exit 1
+fi
+
+# Session Manager Pluginのインストール
+echo "インストール中..."
+sudo ./sessionmanager-bundle/install -i /usr/local/sessionmanagerplugin -b /usr/local/bin/session-manager-plugin
+
+if [ $? -ne 0 ]; then
+    echo "Failed to install Session Manager Plugin"
+    exit 1
+fi
+
+# インストール確認
+echo "インストール確認中..."
+session-manager-plugin
+
+if [ $? -eq 0 ]; then
+    echo "インストールが成功しました!"
+    # 不要なファイルを削除
+    echo "Cleaning up..."
+    rm -f sessionmanager-bundle.zip
+    rm -rf sessionmanager-bundle
+    echo "Cleanup completed."
+else
+    echo "インストールに失敗しています"
+    exit 1
+fi
+
 
 echo "===== Brewfileを使用したパッケージのインストール ====="
 if [ -f ./Brewfile ]; then
@@ -63,6 +113,7 @@ if [ -f ./Brewfile ]; then
   brew bundle --file=Brewfile
 else
   echo "Brewfileが見つかりません。スクリプトを終了します。"
+  pwd
   exit 1
 fi
 
